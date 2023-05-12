5298cd7128ddfaeb5404d2b0859fd7c7833963d9
yaqubroli <walchuk2018@icloud.com>
2023-05-11 17:20:11 -0700

    !!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use chrono::Utc;
+use lettre_email::EmailBuilder;
+use secrets::secrets::EMAIL_USERNAME;
 use crate::verification::verification::*;
 use crate::secrets::secrets::SPECIAL_SALT_CODE;
 
 pub mod verification;
 pub mod secrets;
+
+async fn send_verification_email(verificationRequest: VerificationRequest, ) -> impl Responder {
+    let email = EmailBuilder::new()
+        .to(format("{}@st-andrews.ac.uk", verificationRequest.email))
+        .from(EMAIL_USERNAME)
+        .subject("Minecraft Verification")
+        .text(format!("Here's your whitelist verification link: https://mc.7800.io/verify/{}", verificationRequest.as_code()))
+        .build();
+}
 
 fn main() {
     let verification_request = VerificationRequest {
